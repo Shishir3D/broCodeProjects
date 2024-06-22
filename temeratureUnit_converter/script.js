63c48e3bf6245844f96a3ec73ec5c18af4312dca
Shishir3D <shishirpoudel2061@gmail.com>
2024-06-22 22:13:23 +0545

    [CREATE]: made some updates on temperature converter

---
+++ temeratureUnit_converter/script.js
@@ -0,0 +1,9 @@
+const textbox = document.getElementById("textbox");
+const toCelcius = document.getElementById("toCelcius");
+const toFarenheit = document.getElementById("toFarenheit");
+
+
+
+function convert(){
+        
+}
