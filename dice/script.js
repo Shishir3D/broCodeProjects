0d047f3151e273805dbf00f680b79be47d717b7c
Shishir3D <shishirpoudel2061@gmail.com>
2024-07-20 07:37:18 +0545

    feat(dice): add respective image to dice rolls

--- dice/script.js
+++ dice/script.js
@@ -1,7 +1,11 @@
 const button = document.getElementById("btn");
 const dice = document.getElementById("number");
+const image = document.getElementById("diceimage");
 
 button.onclick = function () {
     let randomNum = Math.trunc((Math.random() * 6)+1);
     dice.textContent = randomNum
+
+    image.src= `../images/dice${randomNum}.png`
 };
+
